513d20241c8297d89717227bd7bb0263273314e3
unknown <mabrukamustafam@gmail.com>
2023-07-10 21:01:34 +0300

    Answered

--- challenge.js
+++ challenge.js
@@ -1,2 +1,24 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function calculator(num1, num2, operator) {
+    let result;
+    if (operator === "+") {
+        result = num1 + num2;
+    } else if (operator === "-") {
+        result = num1 - num2;
+    } else if (operator === "*") {
+        result = num1 * num2;
+    } else if (operator === "/") {
+        result = num1 / num2;
+    } else {
+        console.log("Invalid operator");
+        return;
+    }
+    console.log(`The result of ${num1} ${operator} ${num2} is ${result}.`);
+}
+calculator(5, 3, "+");
+calculator(5, 3, "-");
+calculator(5, 3, "*");
+calculator(5, 3, "/");
+calculator(5, 3, "%"); 
